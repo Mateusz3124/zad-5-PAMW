cd7ce563a76e29616dcf95d0ab098bdfcb14d3ee
MatSobol <mateuszsobol46@gmail.com>
2023-11-12 19:47:21 +0100

    first commit

---
+++ P05Shop.API/Program.cs
@@ -0,0 +1,38 @@
+using P05Shop.API.Services.ProductService;
+using P06.Shared.Services.ProductService;
+using P06.Shared.Services.BookService;
+using P05Shop.API.Services.BookService;
+using Microsoft.EntityFrameworkCore;
+using P05Shop.API.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IBookService, BookService>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
